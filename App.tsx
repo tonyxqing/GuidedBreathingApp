2560fdb106ba0a0a332c0cc16d4e7e8e22731abd
Tony Qing <tony.qing@tamu.edu>
2022-12-03 00:55:16 -0600

    adding customization

--- App.tsx
+++ App.tsx
@@ -1,20 +1,114 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import GuidedBreathing from './GuidedBreathing'
+import { Button, StyleSheet, View } from 'react-native';
+import GuidedBreathing from './GuidedBreathing';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Box, IconButton, NativeBaseProvider, Slider, Text } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Freeze } from 'react-freeze';
+
+const MenuButton = (navigation: any) => (
+  <IconButton
+    onPress={() => navigation.navigate('Options')}
+    title="Options"
+    _icon={{
+      as: MaterialIcons,
+      name: 'menu',
+    }}
+  />
+);
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const [screen, setScreen] = React.useState('Breathing');
+  const [breaths, setBreaths] = React.useState(35);
+  const [breathLength, setBreathLength] = React.useState(4);
+  const [breathHoldTime, setBreathHoldTime] = React.useState(90);
+
+  const Options = React.useCallback(() => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-evenly',
+        }}>
+        <View style={styles.container}>
+          <Box flexDirection="row" alignItems="center">
+          <Button title="+" onPress={() => setBreaths(breaths => breaths + 5)}/>
+          <Text style={styles.text} textAlign="center">Breaths - {breaths}</Text>
+          <Button title="-" onPress={() => setBreaths(breaths => breaths - 5)}/>
+          </Box>
+        </View>
+        <View style={styles.container}>
+          <Box alignItems="center" w="100%">
+          <Button title="+" onPress={() => setBreathLength(breaths => breaths + 5)}/>
+          
+          <Text style={styles.text} textAlign="center">Breath Length - {breathLength} seconds</Text>
+          <Button title="-" onPress={() => setBreathLength(length => length - 5)}/>
+
+          </Box>
+        </View>
+        <View style={styles.container}>
+          <Box alignItems="center" w="100%">
+          <Button title="+" onPress={() => setBreathHoldTime(time => time + 5)}/>
+
+          <Text textAlign="center">
+            Breath Hold Time - {Math.floor(breathHoldTime / 60)}:
+            {breathHoldTime % 60 < 10
+              ? `0${breathHoldTime % 60}`
+              : breathHoldTime % 60}
+          </Text>
+          <Button title="-" onPress={() => setBreathHoldTime(breaths => breaths - 5)}/>
+        
+          </Box>
+        </View>
+      </View>
+    );
+  }, [breaths, breathHoldTime, breathLength])
+
 
   return (
-    <View style={styles.container}>
-      <GuidedBreathing/>
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerTitleAlign: 'center' }}>
+          <Stack.Screen
+            name="Breathing"
+            component={(props: any) => (
+              <Freeze freeze={screen === 'Options'}>
+                <GuidedBreathing breaths={breaths} breathLength={breathLength} breathHoldTime={breathHoldTime} {...props} />
+              </Freeze>
+            )}
+            options={({ navigation }: any) => ({
+              title: 'Guided Breathing',
+              headerLeft: () => {
+                setScreen('Breathing');
+                return MenuButton(navigation);
+              },
+            })}
+          />
+          <Stack.Screen
+            name="Options"
+            component={Options}
+            options={() => ({
+              title: 'Options',
+              headerLeft: () => {
+                setScreen('Options');
+              },
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)"
-  },
+    alignItems: 'center'
+  }, 
+  text: {
+    padding: "0px 10px"
+  }
 });
--- GuidedBreathing.tsx
+++ GuidedBreathing.tsx
@@ -1,102 +1,182 @@
-import * as React from "react";
-import { Text } from "react-native";
-import Animated, {  interpolate, runOnJS, useAnimatedProps, useAnimatedStyle, useSharedValue, withRepeat, withSequence, withTiming } from "react-native-reanimated";
-import Svg, { Circle } from "react-native-svg";
+import * as React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import Animated, {
+  interpolate,
+  runOnJS,
+  useAnimatedProps,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
+import Svg, { Circle } from 'react-native-svg';
 
-type BreathDirection = "Breathe In" | "Breathe Out" | "Beginning" | "Breath Hold"
+type BreathDirection =
+  | 'Breathe In'
+  | 'Breathe Out'
+  | 'Beginning'
+  | 'Breath Hold';
 
-interface GuidedBreathingProps {
-  breathsTillHold?: number;
-  breathHoldTime?: number;
+interface BreathingProps {
+  navigation: any;
+  route: any;
+  breaths: number;
+  breathLength: number;
+  breathHoldTime: number;
 }
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
-const AnimatedText = Animated.createAnimatedComponent(Text)
-const GuidedBreathing = (props: GuidedBreathingProps) => { 
-  const [textTimer, setTextTimer] = React.useState<number>(0)
-  const [breathDirection, setBreathDirection] = React.useState<BreathDirection>("Beginning")
+const AnimatedText = Animated.createAnimatedComponent(Text);
+
+const GuidedBreathing = (props: BreathingProps) => {
+  const { breaths, breathLength, breathHoldTime } = props;
+  const [textTimer, setTextTimer] = React.useState<number>(0);
+  const [breathDirection, setBreathDirection] =
+    React.useState<BreathDirection>('Beginning');
   const [breathHold, setBreathHold] = React.useState<boolean>(false);
-  const {breathsTillHold = 30, breathHoldTime = 90} = props;
-  const r = useSharedValue(200)
-  const opacity = useSharedValue(1)
-  const counter = useSharedValue(0)
+  const [beginSession, setBeginSession] = React.useState<boolean>(false);
+  const r = useSharedValue(150);
+  const opacity = useSharedValue(1);
+  const counter = useSharedValue(0);
   const animatedProps = useAnimatedProps(() => {
     return {
-      r: r.value
-    }
-  })
+      r: r.value,
+    };
+  });
 
   const animatedStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(r.value, [20,110, 110, 200], [0, 1, 1, 0] )
-    }
-  })
-  
+      opacity: interpolate(r.value, [20, 110, 110, 200], [0, 1, 1, 0]),
+    };
+  });
+
   React.useEffect(() => {
     // opacity.value = withRepeat(withTiming(0, {duration: 750}), 4, false)
-    if (breathHold) return;
+    if (breathHold || !beginSession) return;
     r.value = withSequence(
-       
-      withRepeat( 
-      withTiming(20, {
-        duration: 3000,
-      }, () => {
-        if (counter.value === breathsTillHold * 2) {
-          runOnJS(setBreathHold)(true)
-          return;
-        }
+      withRepeat(
+        withTiming(
+          20,
+          {
+            duration: breathLength * 1000,
+          },
+          () => {
+            if (counter.value === breaths * 2) {
+              runOnJS(setBreathHold)(true);
+              return;
+            }
 
-        if (counter.value % 2 === 0) {
-          runOnJS(setBreathDirection)('Breathe In')
-        } else {
-          runOnJS(setBreathDirection)('Breathe Out')
-        }
+            if (counter.value % 2 === 0) {
+              runOnJS(setBreathDirection)('Breathe In');
+            } else {
+              runOnJS(setBreathDirection)('Breathe Out');
+            }
 
-        counter.value += 1;
-        runOnJS(setTextTimer)(Math.floor(counter.value / 2))
-      }), breathsTillHold * 2 + 1, true
-    ))
-  }, [breathHold])
+            counter.value += 1;
+            runOnJS(setTextTimer)(Math.floor(counter.value / 2));
+          }
+        ),
+        breaths * 2 + 1,
+        true
+      )
+    );
+  }, [breathHold, beginSession]);
 
   const [timer, setTimer] = React.useState(0);
   React.useEffect(() => {
     if (!breathHold) return;
-    setBreathDirection("Breath Hold")
-    opacity.value = withRepeat(withTiming(1, {duration: 5000}), -1, true)
-    const longBreathHold: any = setInterval(() => {setTimer(prevTime => prevTime + 1)}, 1000);
+    setBreathDirection('Breath Hold');
+    opacity.value = withRepeat(withTiming(1, { duration: 5000 }), -1, true);
+    const longBreathHold: any = setInterval(() => {
+      setTimer((prevTime) => prevTime + 1);
+    }, 1000);
     setTimeout(() => {
-    setBreathDirection("Breathe In")
-      r.value = withTiming(200, {duration: 2000}, () => {runOnJS(setBreathDirection)("Breath Hold")})
+      setBreathDirection('Breathe In');
+      r.value = withTiming(150, { duration: 2000 }, () => {
+        runOnJS(setBreathDirection)('Breath Hold');
+      });
       counter.value = 0;
-      setTimer(0)
-      setTextTimer(0)
-      const shortBreathHold: any = setInterval(() => {setTimer(prevTime => prevTime + 1)}, 1000);
-      setTimeout(()=> {setBreathHold(false); setBreathDirection("Breathe Out"); setTimer(0); return clearInterval(shortBreathHold)}, 15000)
-      return clearInterval(longBreathHold)
-    }, breathHoldTime * 1000 + 500)
-  }, [breathHold])
+      setTimer(0);
+      setTextTimer(0);
+      const shortBreathHold: any = setInterval(() => {
+        setTimer((prevTime) => prevTime + 1);
+      }, 1000);
+      setTimeout(() => {
+        setBreathHold(false);
+        setBreathDirection('Breathe Out');
+        setTimer(0);
+        return clearInterval(shortBreathHold);
+      }, 15000);
+      return clearInterval(longBreathHold);
+    }, breathHoldTime * 1000 + 500);
+  }, [breathHold]);
 
   const countDown = (sec: number) => {
-    const m = Math.floor(sec / 60)
-    const s = sec % 60
-    if (s <= 9) return `${m}:0${s}`
-    return `${m}:${s}`
-  }
-  
+    const m = Math.floor(sec / 60);
+    const s = sec % 60;
+    if (s <= 9) return `${m}:0${s}`;
+    return `${m}:${s}`;
+  };
+
   return (
-  <>
-    <AnimatedText style={[{position: 'absolute', top: "17%", fontWeight: "400", fontSize: 50, opacity: opacity.value}, breathHold ? {} : animatedStyle]}>{breathDirection}</AnimatedText>
-    <Svg style={{zIndex: -1}} width={540} height={960} viewBox="0 0 540 960" xmlns="http://www.w3.org/2000/svg" >
-      <AnimatedCircle
-        cx={270}
-        cy={480}
-        r={r.value}
-        fill="rgba(119, 178, 178, 0.8)"
-        {...{animatedProps}}
-        />
-    </Svg>
-    <AnimatedText style={[{position: 'absolute', bottom: "17%", fontWeight: "400", fontSize: 50}]}>{breathHold ? countDown(timer) : textTimer}</AnimatedText>
-  </> 
-)};
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:
+          'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)',
+      }}>
+      {beginSession ? (
+        <>
+          <AnimatedText
+            style={[
+              {
+                position: 'absolute',
+                top: '17%',
+                fontWeight: '400',
+                fontSize: 50,
+                opacity: opacity.value,
+              },
+              breathHold ? { opacity: 0.2 } : animatedStyle,
+            ]}>
+            {breathDirection}
+          </AnimatedText>
+
+          <Svg
+            style={{ zIndex: -1 }}
+            width={540}
+            height={960}
+            viewBox="0 0 540 960"
+            xmlns="http://www.w3.org/2000/svg">
+            <AnimatedCircle
+              cx={270}
+              cy={480}
+              r={r.value}
+              fill="rgba(119, 178, 178, 0.8)"
+              {...{ animatedProps }}
+            />
+          </Svg>
+          <AnimatedText
+            style={{
+              position: 'absolute',
+              bottom: '17%',
+              fontWeight: '400',
+              fontSize: 50,
+            }}>
+            {breathHold ? countDown(timer) : textTimer}
+          </AnimatedText>
+        </>
+      ) : (
+        <View>
+          <TouchableOpacity onPress={() => setBeginSession(true)}>
+            <Text style={{ fontSize: 30 }}>Tap to Begin Session</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+};
 
 export default GuidedBreathing;
